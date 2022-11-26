7dae303092390f5495e017b0a16dd927e1fe78ac
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2022-11-26 22:06:36 +0200

    add a custom progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -34,8 +34,33 @@ const Wrapper = styled.progress`
 
   &::-webkit-progress-value {
     background-color: ${COLORS.primary};
-    border-radius: ${props => props.value >= +props.max - 2 ? 'var(--borderRadius)' : '4px 0 0 4px'};
+    border-radius: ${props => props.value <= +props.max - 2 ? '4px 0 0 4px' : '4px'};
   }
+`;
+
+const CustomWrapper = styled.div`
+  padding: var(--padding);
+  background-color: ${COLORS.transparentGray15};
+  border-radius: var(--borderRadius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  height: var(--hight);
+`;
+
+const Progress = styled.svg`
+  width: 100%;
+  height: 100%;
+  border-radius: ${props => props.value <= +props.max - 2 ? '4px 0 0 4px' : '4px'};
+  display: block;
+
+  ::before {
+    content: '${props => props.value}';
+  }
+`;
+
+const ProgressValue = styled.rect`
+    height: 100%;
+    stroke: ${COLORS.primary};
+    fill: ${COLORS.primary};
 `;
 
 const ProgressBar = ({ value, size }) => {
@@ -44,6 +69,15 @@ const ProgressBar = ({ value, size }) => {
     <>
       <VisuallyHidden id="loadinglabel">Loading: {value}%</VisuallyHidden>
       <Wrapper value={value} max="100" style={styles}>{value}%</Wrapper>
+      <hr />
+      <VisuallyHidden id="loadinglabel">Loading: {value}%</VisuallyHidden>
+      <CustomWrapper style={styles}>
+        <span role="progressbar" aria-labelledby="loadinglabel" aria-valuenow={value}>
+          <Progress value={value} max="100">
+            <ProgressValue width={`${value}%`} />
+          </Progress>
+        </span>
+      </CustomWrapper>
     </>
   );
 };
